e330ca14aa2225bc8839122c5b5a8b64cdb67127
Regionald <regionaldmongwe@gmail.com>
2022-02-03 10:01:11 +0200

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+
+PORT = process.env.PORT || 3501;
+
+app.get('/',(req,res)=>{
+    res.render('home');
+    
+});
+
+app.get('/download/edu',(req,res)=>{
+    
+    res.download('edu.jpg')
+    
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('App is running at port' + PORT);
+  });
